fe0e4aeacb0e86637620374e176d40c2ddf76d5b
Pxlart Design <michael.rhema@gmail.com>
2021-07-17 11:54:30 +0000

    - finally it works

--- common.js
+++ common.js
@@ -6,47 +6,32 @@ let batchedIds = [];
 const MAX_REQUEST_COUNT = 3;
 
 const baseURL =
-"https://europe-west1-quickstart-1573558070219.cloudfunctions.net";
+  "https://europe-west1-quickstart-1573558070219.cloudfunctions.net";
 
-const axiosAltInstance = axios.create({ baseURL })
+const axiosAltInstance = axios.create({ baseURL });
 
 axios.defaults.baseURL = baseURL;
 
-let interval
+let interval;
 
 axios.interceptors.request.use(
   function (config) {
     return new Promise((resolve, reject) => {
       currentRequests = [...currentRequests, ...config.params.ids];
-      interval = interval || setInterval(function () {
-        clearInterval(interval);
-        axiosAltInstance
-          .get('/file-batch-api', {
-            params: {
-              ids: currentRequests,
-            },
-          })
-          // .then(response => console.log(response.data))
-          .then((response) => {
-            return resolve(response.data)
-            unresolvedQueue.forEach((paramStr) => {
-              reject({
-                isQueuedItem: true
-              })
-              // console.log(Object.keys(paramStr)[0]);
-              // const { resolve, reject } = Object.values(paramStr)[0];
-              // resolve({
-              //   items: Object.keys(paramStr)[0]
-              //     .split("-")
-              //     .map((item) =>
-              //       response.data.items.find((respItem) => respItem.id === item)
-              //     ),
-              // });
-            });
-            unresolvedQueue = [];
-            currentRequests = [];
-          });
-      }, 1000);
+      interval =
+        interval ||
+        setInterval(function () {
+          clearInterval(interval);
+          const lastRequestFile = unresolvedQueue.pop()
+          const lastRequest = Object.values(lastRequestFile)[0];
+
+          config.lastRequestFileId = Object.keys(lastRequestFile)[0]
+          config.params = {
+            ids: currentRequests,
+          };
+
+          lastRequest.resolve(config);
+        }, 1000);
 
       unresolvedQueue.push({
         [config.params.ids.join("-")]: { resolve, reject },
@@ -60,20 +45,42 @@ axios.interceptors.request.use(
 
 axios.interceptors.response.use(
   function (response) {
-    console.log("response should be ", response);
-    return response;
+    console.log("response should be ", response.data);
 
-    return Promise.resolve({
-      items:
-        response.data.items.find((respItem) => respItem.id === lastItem) || [],
+    unresolvedQueue.forEach((item, idx, arr) => {
+      const fileId = Object.keys(item)[0];
+      const fileData = Object.values(item)[0];
+
+      fileData.reject({
+        items: fileId
+          .split("-")
+          .map((item) =>
+            response.data.items.find((respItem) => respItem.id === item)
+          ).filter(Boolean),
+      });
     });
+
+
+    unresolvedQueue = [];
+    currentRequests = [];
+
+    return {
+      items: response.config.lastRequestFileId
+        .split("-")
+        .map((item) =>
+          response.data.items.find((respItem) => respItem.id === item)
+        ).filter(Boolean),
+    };
   },
   (error) => {
-    console.log(error)
-    // return Promise.resolve(error)
     return new Promise((resolve, reject) => {
-      resolve(error)
-    })
+      // old school error check
+      if (error instanceof Error) {
+        reject(error)
+      } else {
+        resolve(error);
+      }
+    });
   }
 );
 
